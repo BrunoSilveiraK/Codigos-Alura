6c8d6d8f188bfac22c661394277d281ce355e886
Bruno de Souza Silveira <bruno.si@compasso.com.br>
2023-05-04 18:03:04 -0300

    Adicionando um console log

--- app.js
+++ app.js
@@ -1,2 +1,5 @@
 console.log("Criando o sistema de gerenciamento de jogos")
-console.log("Aplicação se conectando ao banco de dados")           
+console.log("Aplicação se conectando ao banco de dados")  
+console.log("Novo log somente para teste")  
+console.log("Enviando para o git hub")        
+ 
