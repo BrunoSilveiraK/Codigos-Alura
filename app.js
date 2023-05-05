a722441ed1fe27455db5199140a6c81f6e2c1811
Bruno Silveira <51918470+BrunoSilveiraK@users.noreply.github.com>
2023-05-05 17:05:33 -0300

    Adicionando um teste de pull

--- app.js
+++ app.js
@@ -3,5 +3,6 @@ console.log("Aplicação se conectando ao banco de dados")
 console.log("Novo log somente para teste")  
 console.log("Enviando para o git hub")  
 console.log("Teste do pull")     
-console.log("Teste de commit e de push") 
+console.log("Teste de commit e de push")
+console.log("Teste de pull")
 
